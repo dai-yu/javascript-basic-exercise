b16c2311ad07c04cd5598a2462a127148ba42675
dai-yu <1435480344@qq.com>
2020-09-06 12:46:59 +0800

    feat: finish exercise_2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,18 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(string == null || string == undefined || string === ''){
+    return 0;
+  }
+  if(prediction !== undefined) {
+    let r=0;
+    for(let i = 0; i < string.length; i++) {
+      if(prediction(string.charAt(i))){
+        r++;
+      }
+    }
+    return r;
+  }
+
+  return string.length;
 }
