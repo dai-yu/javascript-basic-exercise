9ed8ffd67a89517b3517b65c3652d2ca8de8bd5f
dai-yu <1435480344@qq.com>
2020-09-06 18:10:32 +0800

    feat: finish exercise_5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,24 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  
+  if(array === undefined){
+    throw new Error('Flatten undefined or null array');
+  }else if(array === null){
+    throw new Error('Flatten undefined or null array');
+  }else if(array.length == 0){
+    return array;
+  }
+
+  var result = [];
+  for(let i=0;i<array.length;i++){
+    if(array[i] instanceof Array){
+      for(let j= 0; j<array[i].length;j++){
+        result.push(array[i][j]);
+      }
+    }else{
+      result.push(array[i]);
+    }   
+  }
+  return result;
 }
