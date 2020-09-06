be263306149544fc814ec26be333693c5716c850
dai-yu <1435480344@qq.com>
2020-09-06 17:35:53 +0800

    feat: finish exercise_4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,24 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(array === undefined){
+    throw new Error('Creating list from undefined array');
+  }else if(array === null){
+    throw new Error('Creating list from undefined array');
+  }else if(array.length == 0){
+    throw new Error('Creating list from empty array');
+  }
+
+  function Node (value){
+    this.value = value;
+    this.next = null;
+  }
+
+  const node = new Node(array[0]);
+  var point=node;
+  for(let i = 1 ; i < array.length ; i++ ){
+    point.next = new Node(array[i]);
+    point = point.next;
+  }
+  return node;
 }
