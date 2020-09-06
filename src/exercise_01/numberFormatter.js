771919ac4a8e03d7c78efcddfeca8dddb2e40eb8
dai-yu <1435480344@qq.com>
2020-09-06 11:52:47 +0800

    feat: finish exercise_1

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -8,5 +8,11 @@ export default function formatNumber(number, option) {
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  number = number.toFixed(2);
+  if (option != undefined) {
+    if (option.currency === true){
+      number = '$ ' + number;
+    }
+  }
+  return number;
 }
